c32a549b70d463ddf94c63ab71a77f549140a6ee
Tony-jdev <artpopko28@gmail.com>
2025-03-28 14:11:49 +0200

    Added extract_words function, that works with regular expressions

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,10 @@
+import re
+
 def read_file(filename):
     """Зчитує вміст файлу з вказаною назвою."""
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read().lower()
+
+def extract_words(text):
+    """Розбиває текст на слова, ігноруючи пунктуацію."""
+    return re.findall(r'\b\w+\b', text)
